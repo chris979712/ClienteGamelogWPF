66c392f5bcf30d997d7ff01908fde4b3e5e3cad1
chris <chrisvasquez985@gmail.com>
2025-06-07 23:57:34 -0600

    Cambio baneo de usuario en tiempo real

--- GameLogEscritorio/GameLogEscritorio/Servicios/ServicioNotificacion/controlador/UsuarioNotificacionControlador.cs
+++ GameLogEscritorio/GameLogEscritorio/Servicios/ServicioNotificacion/controlador/UsuarioNotificacionControlador.cs
@@ -1,8 +1,13 @@
-﻿using GameLogEscritorio.Servicios.ServicioNotificacion.Mensaje;
+﻿using GameLogEscritorio.Servicios.GameLogAPIRest.Modelo.ApiResponse;
+using GameLogEscritorio.Servicios.GameLogAPIRest.Modelo.Juegos;
+using GameLogEscritorio.Servicios.GameLogAPIRest.Modelo.RespuestasApi;
+using GameLogEscritorio.Servicios.GameLogAPIRest.Servicio;
+using GameLogEscritorio.Servicios.ServicioNotificacion.Mensaje;
 using GameLogEscritorio.Utilidades;
 using GameLogEscritorio.Ventanas;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +17,8 @@ namespace GameLogEscritorio.Servicios.ServicioNotificacion.controlador
 {
     public class UsuarioNotificacionControlador
     {
+        private static readonly IApiRestRespuestaFactory apiRespuestasRestFactory = new FactoryRespuestasAPI();
+
         public UsuarioNotificacionControlador() 
         {
            
@@ -33,7 +40,7 @@ namespace GameLogEscritorio.Servicios.ServicioNotificacion.controlador
                     break;
                 case Constantes.AccionSocialBanearUsuario:
                     MostrarAdvertencia(notificacion.mensaje!);
-                    await ManejadorSesion.RegresarInicioDeSesionSinAcceso();
+                    await CerrarSesionUsuarioBaneado();
                     break;
             }
         }
@@ -53,6 +60,24 @@ namespace GameLogEscritorio.Servicios.ServicioNotificacion.controlador
             {
                 VentanaEmergente ventanaEmergente = new VentanaEmergente(Constantes.TipoAdvertencia, mensaje, Constantes.CodigoErrorAcceso);
                 AnimacionesVentana.MostarVentanaEnCentroDePosicionActualDeVentana(Estaticas.ultimoTopVentana, Estaticas.ultimoLeftVentana, Estaticas.ultimoWidthVentana, Estaticas.ultimoHeightVentana, ventanaEmergente);
+            });
+        }
+
+        private async Task CerrarSesionUsuarioBaneado()
+        {
+            var respuesta = await ServicioLogin.CerrarSesion(UsuarioSingleton.Instancia.correo!,apiRespuestasRestFactory);
+            UsuarioSingleton.Instancia.CerrarSesion();
+            SesionToken.CerrarSesion();
+            Estaticas.juegosPendientes = new ObservableCollection<JuegoCompleto>();
+            Estaticas.juegosFavoritos = new List<Juego>();
+            Estaticas.idJugadoresSeguido = new List<int>();
+            await ServicioNotificacion.Desconectar();
+            await Application.Current.Dispatcher.InvokeAsync(() =>
+            {
+                var ventanaInicioDeSesion = new VentanaInicioDeSesion();
+                AnimacionesVentana.IniciarVentanaPosicionActualDeVentana(Estaticas.ultimoTopVentana,Estaticas.ultimoLeftVentana,Estaticas.ultimoWidthVentana,Estaticas.ultimoHeightVentana,ventanaInicioDeSesion);
+                Application.Current.MainWindow?.Close();
+                Application.Current.MainWindow = ventanaInicioDeSesion;
             });
         }
 
